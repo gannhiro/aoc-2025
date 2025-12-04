216b6daec4eb9a2cddfe7cbd45d6708759ab2fd9
gannhiro <ethan.lorzano@gmail.com>
2025-12-04 14:40:21 +0800

    day 3 part one done

---
+++ src/day_3.zig
@@ -0,0 +1,73 @@
+const std = @import("std");
+
+pub fn execute(gpa: std.mem.Allocator) !void {
+    const file = try std.fs.cwd().openFile("src/day_3_input.txt", .{});
+    defer file.close();
+
+    const input_text = try file.readToEndAlloc(gpa, 1024 * 1024);
+
+    var lines = std.mem.tokenizeAny(u8, input_text, "\n");
+
+    var total: u64 = 0;
+
+    while (lines.next()) |line| {
+        var first_number_check: u8 = 9;
+        var second_number_check: u8 = 9;
+        var first_num_pos: usize = 0;
+        var second_number_pos: usize = 0;
+
+        var found = false;
+
+        while (first_number_check > 0) {
+            first_num_pos = 0;
+            found = false;
+
+            while (first_num_pos < line.len) {
+                const number = try std.fmt.parseUnsigned(u8, line[first_num_pos .. first_num_pos + 1], 10);
+
+                if (first_number_check == number) {
+                    found = true;
+                    break;
+                }
+
+                first_num_pos += 1;
+            }
+
+            if (found and first_num_pos < line.len - 1) break;
+
+            if (first_number_check == 0) break;
+            first_number_check -= 1;
+        }
+
+        if (first_num_pos != line.len - 1) {
+            second_number_pos = first_num_pos + 1;
+        }
+
+        while (second_number_check > 0) {
+            second_number_pos = first_num_pos + 1;
+            found = false;
+
+            while (second_number_pos < line.len) {
+                const number = try std.fmt.parseUnsigned(u8, line[second_number_pos .. second_number_pos + 1], 10);
+
+                if (second_number_check == number) {
+                    found = true;
+                    break;
+                }
+
+                second_number_pos += 1;
+            }
+
+            if (found) break;
+
+            second_number_check -= 1;
+        }
+
+        const final_number_str = [_]u8{ first_number_check + 48, second_number_check + 48 };
+        const final_number = try std.fmt.parseUnsigned(u8, &final_number_str, 10);
+
+        total += final_number;
+    }
+
+    std.debug.print("{d}", .{total});
+}
--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,11 @@
 const std = @import("std");
 const day_1 = @import("day_1.zig");
 const day_2 = @import("day_2.zig");
+const day_3 = @import("day_3.zig");
 
 pub fn main() !void {
     const gpa = std.heap.page_allocator;
-    try day_1.execute(gpa);
-    try day_2.execute(gpa);
+    // try day_1.execute(gpa);
+    // try day_2.execute(gpa);
+    try day_3.execute(gpa);
 }
