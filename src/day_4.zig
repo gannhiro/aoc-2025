fc1f2e935c62a7428cd7345d0af4cbb34aba9145
gannhiro <ethan.lorzano@gmail.com>
2025-12-06 15:17:21 +0800

    day 4 part 2 finished

--- src/day_4.zig
+++ src/day_4.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const d_print = std.debug.print;
 
-const Coordinates = struct { row: usize, column: usize };
+const Coords = struct { row: u8, column: u8 };
 
 pub fn execute(gpa: std.mem.Allocator) !void {
     const file = try std.fs.cwd().openFile("src/inputs/day_4_input.txt", .{});
@@ -22,49 +22,70 @@ pub fn execute(gpa: std.mem.Allocator) !void {
         counter += 1;
     }
 
-    var row_counter: u8 = 0;
     var total: u64 = 0;
 
     var surrounding_rolls: u8 = 0;
 
-    while (row_counter < 140) {
-        var column_counter: u8 = 0;
+    var rolls_to_be_removed = std.ArrayList(Coords){};
+    defer rolls_to_be_removed.deinit(gpa);
 
-        while (column_counter < 140) {
-            if (paper_shelves[row_counter][column_counter] == '@') {
-                const r_scout_start = if (row_counter > 0) row_counter - 1 else 0;
-                const r_scout_end = if (row_counter < 139) row_counter + 1 else 139;
-                const c_scout_start = if (column_counter > 0) column_counter - 1 else 0;
-                const c_scout_end = if (column_counter < 139) column_counter + 1 else 139;
+    while (true) {
+        var row_counter: u8 = 0;
 
-                var r_counter = r_scout_start;
-                while (r_counter <= r_scout_end) {
+        rolls_to_be_removed.clearRetainingCapacity();
+
+        while (row_counter < 140) {
+            var column_counter: u8 = 0;
+
+            while (column_counter < 140) {
+                if (paper_shelves[row_counter][column_counter] == '@') {
+                    const r_scout_start = if (row_counter > 0) row_counter - 1 else 0;
+                    const r_scout_end = if (row_counter < 139) row_counter + 1 else 139;
+                    const c_scout_start = if (column_counter > 0) column_counter - 1 else 0;
+                    const c_scout_end = if (column_counter < 139) column_counter + 1 else 139;
+
+                    var r_counter = r_scout_start;
                     var c_counter = c_scout_start;
+                    while (r_counter <= r_scout_end) {
+                        c_counter = c_scout_start;
 
-                    while (c_counter <= c_scout_end) : (c_counter += 1) {
-                        if (r_counter == row_counter and c_counter == column_counter) {
-                            continue;
+                        while (c_counter <= c_scout_end) : (c_counter += 1) {
+                            if (r_counter == row_counter and c_counter == column_counter) {
+                                continue;
+                            }
+
+                            if (paper_shelves[r_counter][c_counter] == '@') {
+                                surrounding_rolls += 1;
+                            }
                         }
 
-                        if (paper_shelves[r_counter][c_counter] == '@') {
-                            surrounding_rolls += 1;
-                        }
+                        r_counter += 1;
                     }
 
-                    r_counter += 1;
+                    if (surrounding_rolls < 4) {
+                        try rolls_to_be_removed.append(gpa, .{ .row = row_counter, .column = column_counter });
+                    }
+
+                    surrounding_rolls = 0;
                 }
 
-                if (surrounding_rolls < 4) {
-                    total += 1;
-                }
-
-                surrounding_rolls = 0;
+                column_counter += 1;
             }
 
-            column_counter += 1;
+            row_counter += 1;
         }
 
-        row_counter += 1;
+        const temp_items = rolls_to_be_removed.items;
+
+        if (temp_items.len == 0) {
+            break;
+        } else {
+            total += temp_items.len;
+
+            for (temp_items) |coord| {
+                paper_shelves[coord.row][coord.column] = 'x';
+            }
+        }
     }
 
     std.debug.print("{d}", .{total});
